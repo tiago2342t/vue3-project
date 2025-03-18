6a4f077e732865120c04cd0755b0988a0d4a6b7f
tiago <tiagohig70@gmail.com>
2025-03-18 13:30:47 -0500

    First commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+const name = 'Vue dynamic'
+const styleColor = "color: blue"
+const arrayColors = ['blue', 'red', 'yellow']
+const activo = false
+</script>
+
+<template>
+  <h1>Hello {{ name.toUpperCase() }}!</h1>
+  <h2>{{ arrayColors }}</h2>
+  <h2 :style="`color: ${arrayColors[0]}`">Texto colorido</h2>
+  <h2>
+    {{ activo ? 'Estoy activo' : 'Estoy inactivo' }}
+  </h2>
+</template>
+
+<style>
+h1 {
+  color: red;
+}
+</style>
