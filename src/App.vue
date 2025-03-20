156d0cd3ec3bf8404042f04334cffeb80347ab75
tiago <tiagohig70@gmail.com>
2025-03-20 16:55:46 -0500

    Bootstrap and Composition API exercise

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,68 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from "vue"
 
-let counter = ref(0)
+const name = "Vue 3"
+const counter = ref(0)
+const favorites = ref([])
+
 const increment = () => {
-  console.log('Aumentar contador')
   counter.value++
 }
+
+const decrement = () => {
+  counter.value--
+}
+
+const reset = () => {
+  counter.value = 0
+}
+
+const add = () => {
+  favorites.value.push(counter.value)
+}
+
+const blockAdd = computed(() => {
+  const numSearch = favorites.value.find(num => num === counter.value)
+  return numSearch || numSearch === 0;
+})
+
+const classCounter = computed(() => {
+  if (counter.value === 0) {
+    return "zero"
+  }
+  return counter.value > 0 ? "positive" : "negative";
+})
 </script>
 
 <template>
-  <h2>{{ counter }}</h2>
-  <button @click="increment">Aumentar</button>
+  <div class="container text-center mt-5">
+    <h1>Hola {{ name }}!</h1>
+    <h2 :class="classCounter">
+      {{ counter }}
+    </h2>
+    <div class="btn-group">
+      <button @click="increment" class="btn btn-success">Increment</button>
+      <button @click="decrement" class="btn btn-danger">Decrement</button>
+      <button @click="reset" class="btn btn-secondary">Reset</button>
+      <button @click="add" :disabled="blockAdd" class="btn btn-primary">Add</button>
+    </div>
+    <h2 class="mt-3">Mis Favoritos</h2>
+    <ul class="list-group mt-2">
+      <li class="list-group-item" v-for="(item, index) in favorites" :key="index">{{ item }}</li>
+    </ul>
+  </div>
 </template>
 
 <style>
+.negative {
+  color: red;
+}
 
+.positive {
+  color: green;
+}
+
+.zero {
+  color: black;
+}
 </style>
